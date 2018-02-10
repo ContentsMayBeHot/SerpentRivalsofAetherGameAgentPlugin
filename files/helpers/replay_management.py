cf0d8cb9e12407c746e302fa97b46d9701709b82
mjharrison <mjsonofharry@gmail.com>
2018-02-09 20:10:31 -0500

    Removed replay loader from replay management, as it now belongs to the Supervised Learning repo

--- files/helpers/replay_management.py
+++ files/helpers/replay_management.py
@@ -167,26 +167,3 @@ class ReplayManager:
         '''Mark specified .roa file as visited.'''
         self.dataset_visited.append(roa_fname)
         self.dataset_unvisited.remove(roa_fname)
-
-
-# Reference: https://github.com/sorki/python-mnist/blob/master/mnist/loader.py
-class ReplayLoader:
-    def __init__(self, replays_apath='.'):
-        self.replays_apath = replays_apath
-        self.testing_dname = 'testing'
-        self.training_dname = 'training'
-
-        self.testing_x = []
-        self.testing_y = []
-
-        self.training_x = []
-        self.training_y = []
-
-    def load(self, x_apath, y_apath):
-        pass
-
-    def load_testing(self):
-        return self.testing_x, self.testing_y
-
-    def load_training(self):
-        return self.training_x, self.training_y
